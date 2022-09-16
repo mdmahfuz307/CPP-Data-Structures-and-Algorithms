01733c27db0839455d5bdd3e858f8294a29f5548
Md Mahfuz Rahman Nirob <105499709+mdmahfuz307@users.noreply.github.com>
2022-09-16 17:03:05 +0600

    Add files via upload

---
+++ C++/12_increment_decrement.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <conio.h>
+using namespace std;
+int main()
+{
+    int x = 3;
+
+    int y = x--;
+
+    cout << x << endl;
+    cout << y << endl;
+
+    getch();
+}
