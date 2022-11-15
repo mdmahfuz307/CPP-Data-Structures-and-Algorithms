0da21137454523d8cce6d824292be28e69ad687a
Md Mahfuz Rahman Nirob <105499709+mdmahfuz307@users.noreply.github.com>
2022-11-15 23:44:57 +0600

    Add files via upload

---
+++ C++/39_DefultParameters.cpp
@@ -0,0 +1,15 @@
+#include<iostream> 
+#include <conio.h>
+using namespace std;
+
+void display(int a=29,int b=35)
+{
+    cout << a << endl;
+    cout << b << endl;
+}
+int main(){
+
+    display(25,30);
+
+    getchar();
+}
