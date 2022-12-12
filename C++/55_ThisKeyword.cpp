c0560b9b011bd71bacd523b8afe0bfb7c0ca34f9
Md Mahfuz Rahman Nirob <105499709+mdmahfuz307@users.noreply.github.com>
2022-12-12 21:37:21 +0600

    Add files via upload

---
+++ C++/55_ThisKeyword.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <conio.h>
+using namespace std;
+
+class student
+{
+public:
+    string name;
+
+    student (string name)
+    {
+        this -> name = name;
+    }
+    void display()
+    {
+        cout << name << endl;
+    }
+};
+int main()
+{
+    student s1("Nirob");
+    s1.display();
+    getch();
+}
