ca3dab47059d33cf802c29f9aa94cac7d7a7b3a7
Md Mahfuz Rahman Nirob <105499709+mdmahfuz307@users.noreply.github.com>
2023-09-18 13:11:06 +0600

    01_Factorial.cpp

---
+++ DSA All Topic wise Code/1. Data Structures/03. Recursion/01_Factorial.cpp
@@ -0,0 +1,56 @@
+// ███████╗██╗███╗   ██╗██╗███████╗██╗  ██╗    ██╗    ██╗██╗  ██╗ █████╗ ████████╗    ██╗   ██╗ ██████╗ ██╗   ██╗    ███████╗████████╗ █████╗ ██████╗ ████████╗    ██╗
+// ██╔════╝██║████╗  ██║██║██╔════╝██║  ██║    ██║    ██║██║  ██║██╔══██╗╚══██╔══╝    ╚██╗ ██╔╝██╔═══██╗██║   ██║    ██╔════╝╚══██╔══╝██╔══██╗██╔══██╗╚══██╔══╝    ██║
+// █████╗  ██║██╔██╗ ██║██║███████╗███████║    ██║ █╗ ██║███████║███████║   ██║        ╚████╔╝ ██║   ██║██║   ██║    ███████╗   ██║   ███████║██████╔╝   ██║       ██║
+// ██╔══╝  ██║██║╚██╗██║██║╚════██║██╔══██║    ██║███╗██║██╔══██║██╔══██║   ██║         ╚██╔╝  ██║   ██║██║   ██║    ╚════██║   ██║   ██╔══██║██╔══██╗   ██║       ╚═╝
+// ██║     ██║██║ ╚████║██║███████║██║  ██║    ╚███╔███╔╝██║  ██║██║  ██║   ██║          ██║   ╚██████╔╝╚██████╔╝    ███████║   ██║   ██║  ██║██║  ██║   ██║       ██╗
+// ╚═╝     ╚═╝╚═╝  ╚═══╝╚═╝╚══════╝╚═╝  ╚═╝     ╚══╝╚══╝ ╚═╝  ╚═╝╚═╝  ╚═╝   ╚═╝          ╚═╝    ╚═════╝  ╚═════╝     ╚══════╝   ╚═╝   ╚═╝  ╚═╝╚═╝  ╚═╝   ╚═╝       ╚═╝
+//               
+
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef unsigned long long ull;
+typedef pair<int, int> ii;
+typedef vector<ii> vii;
+typedef vector<int> vi;
+typedef long long ll;
+typedef vector<ll> vll;
+typedef double dl;
+
+const ll mx = 1e5 + 123;
+int ar[mx];
+
+#define pb push_back
+#define endl "\n"
+
+mt19937 rng(chrono::high_resolution_clock::now().time_since_epoch().count());
+
+// -------------------------------------------------- Do Not Disturb --------------------------------------------------//b
+
+
+int factorial(int n){
+
+    //Base Condition
+    if(n == 0)
+    return 1;
+
+    // int smaller = factorial(n-1);
+    // int biggerProblem = n*smaller;
+
+    return n * factorial(n-1);
+
+
+}
+int main()
+{
+    ios_base ::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int n;
+    cin>>n;
+
+    int ans = factorial(n);
+    cout<<ans<<endl;  
+
+   return 0;
+}
