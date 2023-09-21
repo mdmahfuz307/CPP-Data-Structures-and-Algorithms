1796ec033e238d8b637ff072eb9ca9cbfd5d7afe
Md Mahfuz Rahman Nirob <105499709+mdmahfuz307@users.noreply.github.com>
2023-09-21 15:59:14 +0600

    Reverse of a number.cpp

--- C++/Math/Reverse of a number.cpp
+++ C++/Math/Reverse of a number.cpp
@@ -8,10 +8,13 @@ int main() {
     int n;
     cin>>n;
     int revNum = 0;
-    while(n>0){
+    
+    while(n!=0){
         int id = n % 10;
         revNum = (revNum * 10) + id;
         n = n / 10;
     }
     cout<<revNum;
+	
 }
+
