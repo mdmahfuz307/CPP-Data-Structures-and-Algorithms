1da5d4d88394e59462b026dfdbb9e72d10b6ffbb
Md Mahfuz Rahman Nirob <105499709+mdmahfuz307@users.noreply.github.com>
2023-09-21 15:52:13 +0600

    Reverse Integer_LeetCode.cpp

--- C++/Math/Reverse Integer_LeetCode.cpp
+++ C++/Math/Reverse Integer_LeetCode.cpp
@@ -1,12 +1,17 @@
 class Solution {
 public:
     int reverse(int x) {
-        int revNum = 0;
-    while(x!=0){
-        int id = x % 10;
-        revNum = (revNum * 10) + id;
-        x = x / 10;
-    }
-    return revNum;
+     int ans = 0;
+        while (x != 0)
+        {
+            int digit = x % 10;
+            if ((ans > INT_MAX / 10) || (ans < INT_MIN / 10))
+            {
+                return 0;
+            }
+            ans = (ans * 10) + digit;
+            x = x / 10;
+        }
+        return ans;
     }
 };
