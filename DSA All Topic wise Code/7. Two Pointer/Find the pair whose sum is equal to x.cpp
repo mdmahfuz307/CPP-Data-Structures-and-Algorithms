2dcb557ae94d12bba2bfcc8a17fed218210f0822
Md Mahfuz Rahman Nirob <105499709+mdmahfuz307@users.noreply.github.com>
2024-05-18 13:56:46 +0600

    Find the pair whose sum is equal to x.cpp

---
+++ DSA All Topic wise Code/7. Two Pointer/Find the pair whose sum is equal to x.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+bool solve(int ar[],int x,int n)
+{
+    int i =0;
+    int j = n-1;
+    while(i<=j)
+    {
+        int sum = ar[i]+ar[j];
+        if(sum==x)return true;
+        if(sum>x)j--;
+        else i++;
+    }
+    return false;
+}
+
+int main()
+{
+    int n;
+    cin>>n;
+
+    int ar[n+3];
+    for(int i =0;i<n;i++)cin>>ar[i];
+    sort(ar,ar+n);
+
+    int x;
+    cin>>x;
+
+    bool ans = solve(ar,x,n);
+    if(ans==true)cout<<"YES"<<endl;
+    else cout<<"NO"<<endl;
+
+}
