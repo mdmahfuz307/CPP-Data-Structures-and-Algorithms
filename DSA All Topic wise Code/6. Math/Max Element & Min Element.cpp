13c7b66e9403b6aaa72e74d235633309511237cc
Md Mahfuz Rahman Nirob <105499709+mdmahfuz307@users.noreply.github.com>
2024-06-25 19:31:28 +0600

    Max Element & Min Element.cpp

--- DSA All Topic wise Code/6. Math/Max Element & Min Element.cpp
+++ DSA All Topic wise Code/6. Math/Max Element & Min Element.cpp
@@ -3,6 +3,11 @@
 using namespace std;
 int main()
 {
+    /*
+    vector<int>v{2,3,4,2,3,5,6,7,8};
+    cout<<*min_element(v.begin(),v.end())<<endl;
+    cout<<*max_element(v.begin(),v.end())<<endl;
+    */
     vector<int>v = {2,34,23,432,32,52,1,34};
     vector<int>::iterator it = max_element(v.begin(),v.end()); //min_element(v.begin(),v.end());
     cout<<*it<<endl;
