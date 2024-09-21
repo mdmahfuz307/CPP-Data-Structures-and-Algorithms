416ec54116e01c5d152e1a57efb927a60bc51276
mdmahfuz307 <njnirob2019@gmail.com>
2024-09-21 08:36:40 +0600

    Array

---
+++ DSA All Topic wise Code/1. Data Structures/01. Array/Array.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    // Declaration
+    int marks[5] = {99,100,101,24,23};
+    int mark[] = {45,23,4,87,34,45,56,87,4};
+
+
+    // ARRAY Print
+    cout<<marks[0]<<endl;
+    cout<<marks[1]<<endl;
+    cout<<marks[2]<<endl;
+    cout<<marks[3]<<endl;
+    cout<<marks[4]<<endl;
+
+    cout<<"Array Size:"<<" "<<sizeof(marks) / sizeof(int)<<endl;
+
+
+    // Print Array useing Loop
+    int size = 5;
+    int mar[size]=  {349,150,301,94,23};
+    for(int i =0;i<size;i++)
+    {
+        cout<<mar[i]<<endl;
+    }
+
+    return 0;
+}
