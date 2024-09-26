a8e22cdcc72c8716db78118e51842b9ba8a8906d
mdmahfuz307 <njnirob2019@gmail.com>
2024-09-26 14:33:25 +0600

    MaxiMum SubArray Optimize O(n)

---
+++ DSA All Topic wise Code/1. Data Structures/01. Array/MaxiMum SubArray Optimiz.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n = 5;
+    int arr[7] = {5,4,-1,7,8};
+
+    int maxSum = INT_MIN,currSum = 0;
+    
+    for(int val : arr){
+        currSum += val;
+        maxSum = max(currSum,maxSum);
+        if(currSum < 0){
+            currSum = 0;
+        }
+    }
+    cout<<"Max SubArray Sum = "<<maxSum<<endl;
+    return 0;
+}
