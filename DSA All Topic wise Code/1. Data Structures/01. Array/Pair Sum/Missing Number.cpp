7f63d08151382aa528cdf12b414e3a21cd9d9b8a
mdmahfuz307 <njnirob2019@gmail.com>
2024-10-05 19:50:53 +0600

    Array

--- DSA All Topic wise Code/1. Data Structures/01. Array/Pair Sum/Missing Number.cpp
+++ DSA All Topic wise Code/1. Data Structures/01. Array/Pair Sum/Missing Number.cpp
@@ -1,12 +1,9 @@
 //https://www.geeksforgeeks.org/problems/missing-number-in-array1416/1?utm_source=geeksforgeeks&utm_medium=article_practice_tab&utm_campaign=article_practice_tab
-#include <bits/stdc++.h>
+#include <iostream>
 using namespace std;
 
 typedef double dl;
 typedef long long ll;
-typedef vector<int> v;
-typedef vector<ll> vll;
-typedef map<int, int> mp;
 typedef pair<int, int> ii;
 typedef unsigned long long ull;
 
@@ -28,21 +25,9 @@ typedef unsigned long long ull;
 #define max4(a, b, c, d) max(d, max(c, max(a, b)))
 #define min4(a, b, c, d) min(d, min(c, min(a, b)))
 
-const ll mx = 1e5 + 123;
-ll ar[mx];
-
-ll gcd(ll a, ll b)
-{
-    return __gcd(a, b);
-}
-ll lcm(ll a, ll b) { return a * (b / gcd(a, b)); }
-
-mt19937 rng(chrono::high_resolution_clock::now().time_since_epoch().count());
 
 int main()
 {
-    ios_base ::sync_with_stdio(false);
-    cin.tie(NULL);
 
     ll n;
     cin>>n;
